6286cfde47a9574549d7a2439f10dd5bd042449e
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-10-08 12:33:33 +0300

    Add separator for ts context

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -171,9 +171,10 @@ return {
   -- nvim-treesitter-context - Sticky header showing current context
   {
     "nvim-treesitter/nvim-treesitter-context",
-    event = { "BufReadPost", "BufNewFile" },
+    event = "VeryLazy",
     opts = {
       enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      multiwindow = true, -- Enable context for all windows (not just the current one)
       max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
       min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
       line_numbers = true,
@@ -182,7 +183,7 @@ return {
       mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
       -- Separator between context and content. Should be a single character string, like '-'.
       -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-      separator = nil,
+      separator = "─",
       zindex = 20, -- The Z-index of the context window
       on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
     },
